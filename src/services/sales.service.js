e4edc0e3fddd2ab58370453c51864531ab6d81c8
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2022-11-29 01:14:32 -0300

    Adiciona rota no service de sales

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -4,6 +4,10 @@ export function fetchSales() {
     return httpClient.get('/sale/');
 };
 
+export function fetchSalesCommissions(date_init, date_end) {
+    return httpClient.get(`/sale_commissions/?date_init=${date_init}&date_end=${date_end}`);
+};
+
 export function deleteSale(id) {
     return httpClient.del(`/sale/${id}/` );
 };
@@ -11,4 +15,3 @@ export function deleteSale(id) {
 export function fetchNewSale(data) {
     return httpClient.post('/sale/', data);
 };
-
