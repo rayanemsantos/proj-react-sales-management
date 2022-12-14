7db785e5cfc38c70b11be8b473ead513a3b2296a
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2022-12-14 00:28:39 -0300

    Adiciona react toastfy

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ import RoutesManage from './infrastructure/router/routes';
 import Layout from './Layout';
 import Theme from './resource/style/theme';
 
+import { ToastContainer, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
 const App = () => {
     return (
         <Theme>
             <BrowserRouter>
                 <Layout>
                     <RoutesManage/>
+                    <ToastContainer
+                        closeOnClick
+                        draggable
+                        newestOnTop
+                        hideProgressBar
+                        autoClose={5000}
+                        transition={Zoom}
+                        position='bottom-right'
+                        theme='colored'
+                    />      
                 </Layout>
             </BrowserRouter>
         </Theme>
